50ebe6cf791a245a7b4f1b3ba7ac73ed5c55cca9
adityasihag1996 <adityasihag1996@gmail.com>
2023-11-20 16:17:23 +0530

    Model training

--- train.py
+++ train.py
@@ -14,8 +14,9 @@ from utils import evaluate_mean_iou
 
 
 def runner(model, train_dataloader, test_dataloader, optimizer, criterion, device, epochs):
-    model = model.to(device)
-    model.train()
+    # model = model.to(device)
+    # model.train()
+    # need to put model in training mode
 
     for epoch in range(epochs):
         running_loss = 0.0
